744fc15e5350f68300932a8a915ecb93c101ad79
Andre Cunningham <andrecunningham62@gmail.com>
2020-07-18 15:44:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
@@ -44,3 +45,33 @@ class Song
       artist_count
   end
 end
+=======
+# class Song
+#   attr_accessor :name, :artist, :genre
+  
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+    
+#   end
+ 
+#   def song
+#     @song = song
+#     song.count
+#   end
+  
+# end 
+ attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+    def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@count +=1
+      @@genres << genre
+      @@artists << artist
+    end
+>>>>>>> 973f724ecaf8fb3a5ac66eb795b241fd750a2c05
